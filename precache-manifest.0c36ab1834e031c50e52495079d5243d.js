53bce1033791e8579468f3cc788a85463d9c3ae8
Juan Carlos Paiva Alcalá <jpaivaalcala@gmail.com>
2020-09-29 21:41:45 -0400

    Updates

---
+++ precache-manifest.0c36ab1834e031c50e52495079d5243d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5916d418434a9a8f58af170d71bff1c3",
+    "url": "/robot-family/index.html"
+  },
+  {
+    "revision": "2e9986fb9f4d3098801b",
+    "url": "/robot-family/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4c51799582196dacdfe9",
+    "url": "/robot-family/static/css/main.5b322147.chunk.css"
+  },
+  {
+    "revision": "2e9986fb9f4d3098801b",
+    "url": "/robot-family/static/js/2.09ded285.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/robot-family/static/js/2.09ded285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c51799582196dacdfe9",
+    "url": "/robot-family/static/js/main.34b29754.chunk.js"
+  },
+  {
+    "revision": "06d815b15f9b5b85b0cb",
+    "url": "/robot-family/static/js/runtime-main.e8ac8f0b.js"
+  },
+  {
+    "revision": "a05201dde127f3f0fcf177f2392b2ecf",
+    "url": "/robot-family/static/media/advanced_led_board-7.a05201dd.woff"
+  }
+]);
