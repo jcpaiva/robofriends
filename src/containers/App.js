1db52faefca8152ba8864d3abce15e17938c4073
Juan Carlos Paiva Alcalá <jpaivaalcala@gmail.com>
2020-09-29 21:44:09 -0400

    fixes

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary"
 import { robots } from "../robots";
 import "./App.css";
 
@@ -36,9 +37,11 @@ class App extends Component {
           <h1 className="f1">RobotFamily</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
-        </div>
+        </div >
       );
   }
 }
